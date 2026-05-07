da95d471cf9493a6f44ed4c4057fcfa0e91194b4
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-05-08 01:48:00 +0200

    Break up Registry object tests into separate file

--- workspace/src/KasaTests.cpp
+++ workspace/src/KasaTests.cpp
@@ -4,66 +4,6 @@
 
 TEST(
   KasaTests,
-  ProductRegistration_RandomizedProductCount_RegistrySizeMatchesGeneratedProductCount)
-{
-    Registry registry;
-    std::random_device my_random_device;
-    std::mt19937 my_generator(my_random_device());
-    std::uniform_int_distribution<> distr(1, 100);
-    int random_product_count = distr(my_generator);
-
-    double price = 10.00;
-    long identifier_aplifier = 10;
-    for (int i = 0; i < random_product_count; ++i)
-    {
-        struct Product temp_product;
-        temp_product.identifier = static_cast<long>(i * identifier_aplifier);
-        temp_product.name = "Product";
-        temp_product.price = price;
-        registry.add(temp_product);
-    }
-    EXPECT_EQ(registry.getEntryCount(), random_product_count);
-}
-
-TEST(KasaTests,
-     ProductRegistration_IgnoreProductOfDuplicateId_ProductNotAddedToRegistry)
-{
-    Registry registry;
-    long duplicate_identifier = 1;
-    double random_price = 67.00;
-    struct Product product_a(
-      duplicate_identifier, "MyUniqueProduct", random_price);
-    struct Product product_b(
-      duplicate_identifier, "AnotherTotallyDifferentProduct", random_price);
-    registry.add(product_a);
-    registry.add(product_b);
-    EXPECT_EQ(registry.getEntryCount(), 1);
-}
-
-TEST(
-  KasaTests,
-  ProductDeregistration_RegistrySizeAfterDeletion_RegistrySizeIsDecrementedAfterDeletingOneProduct)
-{
-    Registry registry;
-    std::random_device my_random_device;
-    std::mt19937 my_generator(my_random_device());
-    std::uniform_int_distribution<> distr(1, 100);
-    int random_product_count = distr(my_generator);
-
-    for (int i = 0; i < random_product_count; ++i)
-    {
-        struct Product temp_product;
-        temp_product.identifier = i + 10;
-        temp_product.name = "Product";
-        temp_product.price = 10.00;
-        registry.add(temp_product);
-    }
-    registry.del(registry.contents.begin()->second.identifier);
-    EXPECT_EQ(registry.getEntryCount(), random_product_count - 1);
-}
-
-TEST(
-  KasaTests,
   ProductCartAddition_AddingRegisteredProduct_EmptyCartGainsSizeAfterAddingRegisteredProduct)
 {
     Registry registry;
